fbd302f2f56d11a0b2540a15b8db2fb0c3f1cb56
Shohkrukh Turaev <turaevshokhrukh7@gmail.com>
2023-02-19 18:32:52 +0500

    remove swr-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { SWRDevTools } from "swr-devtools";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <SWRDevTools>
-      <App />
-    </SWRDevTools>
+    <App />
   </React.StrictMode>
 );
