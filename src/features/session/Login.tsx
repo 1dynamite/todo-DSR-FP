944a34eae3fdd5ac4295c4dda4de8b8e70fa596f
Shohkrukh Turaev <turaevshokhrukh7@gmail.com>
2023-02-28 01:09:31 +0500

    argument against using useCallback

--- src/features/session/Login.tsx
+++ src/features/session/Login.tsx
@@ -13,6 +13,14 @@ export default function Login() {
 
   if (session) return <Navigate to="/todos" />;
 
+  /*  
+    I really don't see why handleSubmit needs to be wrapped in useCallback. I am not sure, but 
+    here are my thoughts.
+    1. This login component rarely re-renders, only once or twice, so optimizing re-renders
+       would be very premature.
+    2. To prevent re-renders of Form element, and for useCallback to work, i would have to
+       wrap the Form element in memo, which is a lot of work for so small improvement in performance.
+ */
   const handleSubmit = async (data: SerializedFormData) => {
     const loginData = data as { login: string; password: string };
 
